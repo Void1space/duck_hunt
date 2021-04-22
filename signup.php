08ef1579f203be4f44ea2a7ac2cc82ebd7782e9b
Void1space <info@voidspaceled.com>
2021-04-21 21:23:18 -0700

    email checker

--- signup.php
+++ signup.php
@@ -20,14 +20,17 @@ if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['phoneNumber'
     $phoneNumber = $_POST['phone_number'];
     $payment = $_POST['payment'];
     // email message
+    $subject = "pwease work";
     $to = $email; 
     $msg = "Duck Hunter Info \n Your name: $name \n Email: $email \n Phone: $phoneNumber \n Cashapp / Venmo: $payment \n Contest Code: $contest_code";
     $headers = "From: duckhunt@eternallux.com" . "\r\n" .
     "Reply-To: duckhunt@eternmallux.com" . "\r\n" .
-    "Reply-To: duckhunt@eternmallux.com" . "\r\n" .
     "CC: voidspacelighting@gmail.com" . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+    "X-Mailer: PHP/" . phpversion();
+    $send_email = mail($to,$subject,$msg,$headers);
 
+    echo ($send_email) ? 'success' : 'error';
+    mail($to,$name,$msg,$headers);
     if(!empty($name) && !empty($email) && !empty($phoneNumber) && !empty($payment)){
 
         $response['message'] = "Success.";
@@ -44,7 +47,7 @@ if(isset($_POST['name']) || isset($_POST['email']) || isset($_POST['phoneNumber'
                 $response['message'] = "Error occured, please try again.";
                 $response['status'] = 0;
             }
-            mail($to,$name,$msg,$headers);
+            
 
         // if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
         //     $response['message'] = "Invalid Email";
